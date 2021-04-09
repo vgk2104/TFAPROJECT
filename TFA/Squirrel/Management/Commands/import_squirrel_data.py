4261c7c05b09e7a2f6bd75e29fdf3775082709bd
vgk2104 <81593176+vgk2104@users.noreply.github.com>
2021-04-09 00:45:17 -0400

    Updated Management Commands

--- TFA/Squirrel/Management/Commands/import_squirrel_data.py
+++ TFA/Squirrel/Management/Commands/import_squirrel_data.py
@@ -1,52 +1,54 @@
 import csv
 from django.core.management.base import BaseCommand
 from dateutil import parser
+import re
 from sightings.models import Squirrel
 from datetime import date
+
 
 class Command(BaseCommand):
     help = 'Import squirrel data file to the database'
 
-    def add_arguments(self,parser):
-        parser.add_argument('path', type =str)
+    def add_arguments(self, parser):
+        parser.add_argument('path', type=str)
 
     def handle(self, *args, **kwargs):
         path = kwargs['path']
 
-        try:
-            with open(path, encoding='utf-8') as f:
-                excel_reader = csv.DictReader(f)
-                for row in excel_reader:
-                    s = Squirrels(
+        pattern = re.compile(r'(\d{2})(\d{2})(\d{4})')
+
+        with open(path, 'rt') as f:
+            excel_reader = csv.reader(f, dialect='excel')
+            next(excel_reader)
+            squirrel_tracker = list()
+            for row in excel_reader:
+                if row[2] in squirrel_tracker:
+                    continue
+                else:
+                    month, day, year = pattern.match(row[5]).groups()
+                    squirrel = Squirrel.objects.get_or_create(
                         X=row['X'],
                         Y=row['Y'],
                         Unique_squirrel_id=row['Unique Squirrel ID'],
                         Shift=row['Shift'],
-                        Date=datetime.date(
-                            int(row['Date'][-4:]), int(row['Date'][:2]), int(row['Date'][2:4])),
+                        Date=date(int(year), int(month), int(day)),
                         Age=row['Age'],
                         Primary_Fur_Color=row['Primary Fur Color'],
                         Location=row['Location'],
                         Specific_location=row['Specific Location'],
-                        Running=row['Running'].upper(),
-                        Chasing=row['Chasing'].upper(),
-                        Climbing=row['Climbing'].upper(),
-                        Eating=row['Eating'].upper(),
-                        Foraging=row['Foraging'].upper(),
-                        Other_activities=row['Other Activities'],
-                        Kuks=row['Kuks'].upper(),
-                        Quaas=row['Quaas'].upper(),
-                        Moans=row['Moans'].upper(),
-                        Tail_flags=row['Tail flags'].upper(),
-                        Tail_twitches=row['Tail twitches'].upper(),
-                        Approaches=row['Approaches'].upper(),
-                        Indifferent=row['Indifferent'].upper(),
-                        Runs_from=row['Runs from'].upper(),
+                        Running=True if row['Running'] == 'TRUE' else False,
+                        Chasing=True if row['Chasing'] == 'TRUE' else False,
+                        Climbing=True if row['Climbing'] == 'TRUE' else False,
+                        Eating=True if row['Eating'] == 'TRUE' else False,
+                        Foraging=True if row['Foraging'] == 'TRUE' else False,
+                        Other_Activities=True if row['Other Activities'] == 'TRUE' else False,
+                        Kuks=True if row['Kuks'] == 'TRUE' else False,
+                        Quaas=True if row['Quaas'] == 'TRUE' else False,
+                        Moans=True if row['Moans'] == 'TRUE' else False,
+                        Tail_Flags=True if row['Tail flags'] == 'TRUE' else False,
+                        Tail_Twitches=True if row['Tail twitches'] == 'TRUE' else False,
+                        Approaches=True if row['Approaches'] == 'TRUE' else False,
+                        Indifferent=True if row['Indifferent'] == 'TRUE' else False,
+                        Runs_From=True if row['Runs from'] == 'TRUE' else False
                     )
-                    s.save()
-        except csv.Error as e:
-            print(f'there is something wrong with {excel_reader.line_num}')
-
-
-
-
+                    squirrel_tracker.append(row[2])
