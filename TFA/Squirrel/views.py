fa59dcc08b0ad12a9ac3ccad841629f58412a7ac
Shantanu Kashikar <sck2181@columbia.edu>
2021-04-09 17:05:58 -0400

    working map feature
    all feature in map are working

--- TFA/Squirrel/views.py
+++ TFA/Squirrel/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from Squirrel.models import Squirrel
 
-def map_(request):
-    return render(request, 'Squirrel/map.html')
+
+def map(request):
+    squirrels = list(Squirrel.objects.all())[:100]
+    context = {'squirrels': squirrels}
+    return render(request, 'Squirrel/map.html', context)
--- TFA/TFA/urls.py
+++ TFA/TFA/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Squirrel.views import map
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', map, name='map'),
 ]
